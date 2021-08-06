0ea9b6b853817e6ea255269dd1033d7982e70f9e
Kirusanth <iamkirushik@gmail.com>
2021-08-06 12:00:25 +0530

    component added

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/BuilderFE'));
+app.use(express.static(__dirname + '/dist/BuilderFE'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/BuilderFE/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/BuilderFE/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
