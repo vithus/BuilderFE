0f3ce1c89e8615ce711aba48cd7fdbb961755ce0
Kirusanth <iamkirushik@gmail.com>
2021-08-06 15:22:14 +0530

    initial comt

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/shan-template'));
+app.use(express.static('./dist/shan-template'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/shan-template/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/shan-template/' }),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
