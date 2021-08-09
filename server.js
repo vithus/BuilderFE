cdad55b32cd13e80ba7db84fb4827d19054a7ad7
Vithushan Yogarajah <shan@unicomtechnology.co.uk>
2021-08-09 17:25:10 +0530

    Commit

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/shan-template'));
+app.use(express.static('./dist'));
 
-//app.get('/*', function (req, res) {
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', { root: 'dist/' }),
+);
 
-    app.get('*', function (req, res) {
-        const index = path.join(__dirname, 'shan-template', 'index.html');
-        res.sendFile(index);
-    });
-
-    // Start the app by listening on the default Heroku port
-    app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
