8533ee66f91ebf6377d6e6579185c57e13e8afbc
Vithushan <vithus209@gmail.com>
2021-08-09 17:06:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-// //Install express server
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-
-// // Serve only the static files form the dist directory
-// app.use(express.static('./dist/shan-template'));
-
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', { root: 'dist/shan-template/' }),
-// );
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -20,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/shan-template'));
+app.use(express.static('./dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/shan-template/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/' }),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
